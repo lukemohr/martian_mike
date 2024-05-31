6c634fd5e9e003614cb3e715dcf2e7f9a7de730f
lukemohr <lukemohr@gmail.com>
2024-05-31 17:33:21 -0400

    update 20240531
    up to and including deathzone

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,16 +7,34 @@ extends CharacterBody2D
 @onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
-	if is_on_floor() == false:
+	if not is_on_floor():
 		velocity.y += gravity * delta
 		if velocity.y > 500:
 			velocity.y = 500
 		
-	if Input.is_action_just_pressed("jump"):
+	if Input.is_action_just_pressed("jump"):# and is_on_floor():
 		velocity.y = -jump_force
 		
 	var direction = Input.get_axis("move_left", "move_right")
+	if direction != 0:
+		animated_sprite.flip_h = (direction == -1)
 	
 	velocity.x = direction * speed
 	
 	move_and_slide()
+	
+	update_animations(direction)
+	
+func update_animations(direction):
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	else:
+		if velocity.y < 0:
+			animated_sprite.play("jump")
+		else:
+			animated_sprite.play("fall")
+	
+
